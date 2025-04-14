a94e10fe3582b5b6ce401b8dd7b716e2fadc8986
kingsofcoderz <gopinathmanjula7@gmail.com>
2025-04-14 11:14:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_socketio import SocketIO, send
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecret'
+app.secret_key = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
-@app.route('/')
-def chat():
-    return render_template('chat.html')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
+# signup route
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('signup.html')
+
+# login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            session['username'] = user.username
+            return redirect('/chat')
+        else:
+            return 'Invalid credentials'
+    return render_template('login.html')
+
+# chat route
+@app.route('/chat')
+def chat():
+    if 'username' in session:
+        return render_template('chat.html', username=session['username'])
+    return redirect('/login')
+
+# socketio message event
 @socketio.on('message')
 def handle_message(msg):
     print('Message: ' + msg)
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
+    db.create_all()  # create db tables
     socketio.run(app, debug=True)
